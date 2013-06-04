849b32c88bc928125315a0caf106b0dd90424cd7
Hoon Cho <soohoon90@gmail.com>
2013-06-04 18:12:37 -0400

    adding category, localizing font

--- js/app.js
+++ js/app.js
@@ -1,19 +1,47 @@
-$(function(){
+function showCategories(dest){
+	for (c in iData){
+		$("<a>").html(c).appendTo(dest);
+		//var items = iData[c];
+	}
+};
 
-  $(document).on("click","#mainContent .items a", function(){
-	var t = $(this).html();
-	$("<li>").html(t).appendTo($("#sidelist"));
-  });
+function showItemsForCategory(c, dest){
+	items = iData[c];
 
-  var dest = $("#mainContent")
-  for (c in iData){
-	var items = iData[c];
-	var u = $("<div>").attr("class", "items").addClass("clearfix").appendTo(dest);
-	$("<h3>").html(c).appendTo(u);
+	// create div
+	var u = $("<div>").attr("class", "items").addClass("clearfix");
+	
+	// populate div
 	for (i in items){
 		var price = iData[c][i];
 		$("<a>").html("<p>"+i+"</p>").appendTo(u);
 	}
-  }
-  $("#debug").html(JSON.stringify(iData, "", 2));
+
+	// add div to dest
+	dest.html(u);
+}
+
+$(function(){
+
+  $(document).on("click","#mainContentItems .items a", function(){
+	var t = $(this).html();
+	var tt = $("#mainSideItems a.selected").html();
+	$("<li>").html(t+" - "+tt).appendTo($("#sidelist"));
+  });
+
+  $(document).on("click","#mainSideItems a", function(){
+  	$("#mainSideItems a").removeClass("selected");
+  	$(this).addClass("selected");
+  	var t = $(this).html();
+  	showItemsForCategory(t,$("#mainContentItems"));
+  });
+
+  showCategories($("#mainSideItems"));
+	// var u = $("<div>").attr("class", "items").addClass("clearfix").appendTo(dest);
+	// for (i in items){
+	// 	var price = iData[c][i];
+	// 	$("<a>").html("<p>"+i+"</p>").appendTo(u);
+	// }
+
+  $("#debugData").html(JSON.stringify(iData, "", 2));
 });
